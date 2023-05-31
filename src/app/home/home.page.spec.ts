aacebb699b91cb92172ec49eefeaf6612e99d775
Allan1503 <allan.152009@hotmail.com>
2023-05-31 17:06:16 -0300

    finalizado

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -10,7 +10,7 @@ describe('HomePage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HomePage],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
