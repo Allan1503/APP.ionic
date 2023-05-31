70d95a7e8a9613ad95ed5ec23361131bf2c5d9ef
Allan1503 <allan.152009@hotmail.com>
2023-05-31 13:37:12 -0300

    ultimos ajustes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonModal) modal: IonModal | undefined;
 
-  constructor() {}
+  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  name: string | undefined;
+  info: string | undefined;
+  address: string | undefined;
 
+  cancel() {
+    this?.modal?.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    const data = {
+      name: this.name,
+      address: this.address,
+      info: this.info,
+    }
+    console.log(data)
+    this?.modal?.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
 }
